31e3b6c18cd5bb8ced6e9a978da0ca0da3f41b8c
SHA888 <kresnasucandra@gmail.com>
2021-05-29 14:45:58 +0800

    Updated Dex.sol function

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -21,6 +21,7 @@ contract Dex is Wallet {
        bytes32 ticker;
        uint amount;
        uint price;
+       uint filled;
     }
 
     uint public nextOrderId = 0;
@@ -77,7 +78,27 @@ contract Dex is Wallet {
     }
 
     function createMarketOrder(Side side, bytes32 ticker, uint amount) public {
-        
+        require(balances[msg.sender][ticker] >= amount, "Insufficient balance");
+        uint orderBookSide;
+        if (side == Side.BUY) {
+            orderBookSide = 1;
+        }
+        else {
+            orderBookSide = 0;
+        }
+        Order[] storage orders = orderBook[ticker][orderBookSide];
+
+        uint totalFilled;
+
+        for (uint256 i = 0; i < orders.length && totalFilled < amount; i++) {
+            // How much we can fill from order[i]
+            // Update totalFilled;
+
+            // Execute the trade & shift balances between buyer/seller
+            // Verify that the buyer has enough ETH to cover
+        }        
+
+        // Loop through the orderbook and remove 100% filled orders
     }
 
 }
